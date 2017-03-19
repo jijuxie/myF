fca8985fa571bfc7aca7f0139d1d876a62e6b5a8
unknown <>
2017-03-19 12:13:38 +0800

    PDO

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 12156
+ * Date: 2017/3/18
+ * Time: 22:01
+ */
+require 'jijuxie_config.php';
+
+$PDO = new \aaaaa\PDOmysql();
---
+++ jijuxie_config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PWD','');
+define('DB_NAME','tp3');
+define('DB_PORT',3306);
+define('DB_TYPE','mysql');
